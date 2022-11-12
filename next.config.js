bed8e89792b76a04c0e88e98e4f687583e4b9e7c
John Doe <doe007800@gmail.com>
2022-11-12 17:00:15 -0500

    fix next config 6

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: '/venarus',
-  basePath: '/venarus',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   //swcMinify: true,
 };
 
